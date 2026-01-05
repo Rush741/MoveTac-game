97f384ec2394321fddf2d953eb335c17f081a539
Rushikesh Jadhav <rajadhav147@gmail.com>
2026-01-06 01:36:56 +0530

    completed basic ui

--- src/Components/MoveTac/MoveTac.jsx
+++ src/Components/MoveTac/MoveTac.jsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import "./MoveTac.css";
 
 const MoveTac = () => {
   return (
-    <div>MoveTac</div>
+    <div>
+        <div className="container">
+            <h1 className="title">Move Tac</h1>
+            <div className="board">
+                <div className="row1">
+                    <div className="boxes"></div>
+                    <div className="boxes"></div>
+                    <div className="boxes"></div>
+                </div>
+                <div className="row2">
+                    <div className="boxes"></div>
+                    <div className="boxes"></div>
+                    <div className="boxes"></div>
+                </div>
+                <div className="row3">
+                    <div className="boxes"></div>
+                    <div className="boxes"></div>
+                    <div className="boxes"></div>
+                </div>
+            </div>
+            <button className="reset">Reset</button>
+        </div>
+    </div>
   )
 }
 
